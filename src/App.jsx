512454e065ec234c06310d8a363ed49fe358ac8d
engsaidabdi <flaskcourse931@gmail.com>
2025-09-28 11:47:53 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-700  p-10 mt-10 ml-30 mr-10'>
+      <h1 className='flex justify-center text-2xl text-white'>welcome to react</h1>
+    </div>
+  )
+}
+
+export default App
