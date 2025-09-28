39ea463296adfea89cee778d56ceb27a4a96e8ea
engsaidabdi <flaskcourse931@gmail.com>
2025-09-28 11:55:46 -0700

    centering

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-blue-700  p-10 mt-10 ml-30 mr-10 flex justify-center '>
+    <div className='bg-blue-700  p-10 mt-10 ml-30 mr-10 flex justify-center items-center '>
       <h1 className='text-2xl text-white'>welcome to react</h1>
       <p>welcome to react js tutorial</p>
     </div>
